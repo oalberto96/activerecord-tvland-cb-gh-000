3f6a9bcfa9cf5a3e169d4e4cc88e706c65f4d499
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-15 18:23:53 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
 
-  def build_network(name)
-    binding.pry
+  def build_network(call_letters)
+    network = Network.new(call_letters)
   end
 end
